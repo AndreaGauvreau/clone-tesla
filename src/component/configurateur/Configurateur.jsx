b8b8634563d1418369b96dcfc13a5dc340be209b
Andrea.Gauvreau <andrea@teachizi.fr>
2023-03-30 16:51:34 +0200

    small correction

--- src/component/configurateur/Configurateur.jsx
+++ src/component/configurateur/Configurateur.jsx
@@ -2,6 +2,7 @@ import {WarningIcon} from '@chakra-ui/icons'
 import {Box, Button, Flex, Heading, Text} from '@chakra-ui/react'
 import React from 'react'
 import {colors} from '../ui/color'
+import MenuConfig from '../ui/MenuConfig'
 import PerformanceDetail from './ConfigComp/PerformanceDetail'
 import SelectColors from './ConfigComp/SelectColors'
 import SelectedWheels from './ConfigComp/SelectedWheels'
@@ -45,151 +46,165 @@ export default function Configurateur({
     )
   }
   return (
-    <Flex
-      flexDirection={{base: 'column', lg: 'row'}}
-      w={'100vw'}
-      maxH={{base: 'none', lg: '100vh'}}
-      minH={'100vh'}
-      overflowY={{base: 'scroll', lg: 'none'}}
-    >
+    <>
+      <MenuConfig />
       <Flex
-        w={{base: '100%', lg: 'full'}}
-        h={'100vh'}
-        objectFit="cover"
-        position={'relative'}
+        flexDirection={{base: 'column', lg: 'row'}}
+        w={'100vw'}
+        maxH={{base: 'none', lg: '100vh'}}
+        minH={'100vh'}
+        overflowY={{base: 'scroll', lg: 'hidden'}}
       >
-        <SliderConfig
-          listImage={listImage}
-          setSliderIndex={setSliderIndex}
-          state={state}
-        />
-      </Flex>
-      <Flex
-        w={{base: '100%', lg: '600px'}}
-        h={'100vh'}
-        overflowY={{base: 'auto', lg: 'scroll'}}
-        p={5}
-        px={10}
-        flexDirection={'column'}
-        alignItems="center"
-        gap={'60px'}
-      >
-        <Flex boxShadow={'lg'} h={'auto'} p={3}>
-          <WarningIcon pt={2} boxSize={7} color={'#3E6AE1'} />
-          <Text fontSize={'sm'}>
-            Prenez livraison d'ici le 31 mars 2023 et bénéficiez de 5 000 km de
-            Supercharge gratuite. Parcourez nos véhicules disponibles.
-          </Text>
+        <Flex
+          w={{base: '100%', lg: 'full'}}
+          h={'100vh'}
+          objectFit="cover"
+          position={'relative'}
+        >
+          <SliderConfig
+            listImage={listImage}
+            setSliderIndex={setSliderIndex}
+            state={state}
+          />
         </Flex>
-        <Flex flexDirection={'column'} alignItems="center">
-          <Heading variant={'headModel'}>Model 3</Heading>
-          <Text variant={'lightSubtile'}>
-            Livraison prévue : Avr – Juin 2023
-          </Text>
-        </Flex>
-        <Flex flexDirection={'row'} alignItems="flex-start" gap={7}>
-          <Flex flexDirection={'column'}>
-            <Flex
-              flexDirection={'row'}
-              alignItems={'flex-end'}
-              justifyContent="center"
-            >
-              <Text variant={'mediumFont'}>{car?.autonomie}</Text>
-              <Text variant={'smallFont'}>Km</Text>
-            </Flex>
-            <Flex
-              flexDirection={'column'}
-              alignItems={'center'}
-              justifyContent="center"
-              h={'50px'}
-            >
-              <Text variant={'tinyFont'}>autonomie</Text>
-              <Text variant={'tinyFont'}>(WLTP)</Text>
-            </Flex>
+        <Flex
+          w={{base: '100%', lg: '600px'}}
+          h={'100vh'}
+          overflowY={{base: 'auto', lg: 'scroll'}}
+          p={5}
+          px={10}
+          flexDirection={'column'}
+          alignItems="center"
+          gap={'60px'}
+          pt={'100px'}
+        >
+          <Flex boxShadow={'lg'} h={'auto'} p={3}>
+            <WarningIcon pt={2} boxSize={7} color={'#3E6AE1'} />
+            <Text fontSize={'sm'}>
+              Prenez livraison d'ici le 31 mars 2023 et bénéficiez de 5 000 km
+              de Supercharge gratuite. Parcourez nos véhicules disponibles.
+            </Text>
           </Flex>
-          <Flex flexDirection={'column'}>
-            <Flex
-              flexDirection={'row'}
-              alignItems={'flex-end'}
-              justifyContent="center"
-            >
-              <Text variant={'mediumFont'}>{car?.vitesse}</Text>
-              <Text variant={'smallFont'}>Km/h</Text>
-            </Flex>
-            <Flex
-              flexDirection={'column'}
-              alignItems={'center'}
-              justifyContent="center"
-              h={'50px'}
-            >
-              <Text variant={'tinyFont'}>vitesse</Text>
-              <Text variant={'tinyFont'}>maximale</Text>
-            </Flex>
-          </Flex>
-          <Flex flexDirection={'column'}>
-            <Flex
-              flexDirection={'row'}
-              alignItems={'flex-end'}
-              justifyContent="center"
-            >
-              <Text variant={'mediumFont'}>{car?.zerocent}</Text>
-              <Text variant={'smallFont'}>s</Text>
-            </Flex>
-            <Flex
-              flexDirection={'column'}
-              alignItems={'center'}
-              justifyContent="center"
-              h={'50px'}
-            >
-              <Text variant={'tinyFont'} textAlign="cetner">
-                0 à 100 km/h
-              </Text>
-            </Flex>
-          </Flex>
-        </Flex>
-        <Flex flexDirection={'column'} w={'100%'} gap={4}>
-          <Text variant={'menuLink'}>Propulsion</Text>
-          <SelectCar carNb={0} />
-          <Text variant={'menuLink'}>Transmission intégrale Dual Motor</Text>
-          <SelectCar carNb={1} />
-          <SelectCar carNb={2} />
           <Flex flexDirection={'column'} alignItems="center">
-            <Text textAlign={'center'} variant={'nolinkText'}>
-              Les véhicules Tesla
+            <Heading variant={'headModel'}>Model 3</Heading>
+            <Text variant={'lightSubtile'}>
+              Livraison prévue : Avr – Juin 2023
             </Text>
-            <Text textAlign={'center'} variant={'linkText'} mt={-1}>
-              appartiennent à la classe énergétique A{' '}
-            </Text>
-            <Text textAlign={'center'} variant={'nolinkText'}>
-              en matière d'émissions de CO2
-            </Text>
-            <Button mt={5} variant={'btnSmall'}>
-              Caractéristiques
-            </Button>
+          </Flex>
+          <Flex flexDirection={'row'} alignItems="flex-start" gap={7}>
+            <Flex flexDirection={'column'}>
+              <Flex
+                flexDirection={'row'}
+                alignItems={'flex-end'}
+                justifyContent="center"
+              >
+                <Text variant={'mediumFont'}>{car?.autonomie}</Text>
+                <Text variant={'smallFont'}>Km</Text>
+              </Flex>
+              <Flex
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent="center"
+                h={'50px'}
+              >
+                <Text variant={'tinyFont'}>autonomie</Text>
+                <Text variant={'tinyFont'}>(WLTP)</Text>
+              </Flex>
+            </Flex>
+            <Flex flexDirection={'column'}>
+              <Flex
+                flexDirection={'row'}
+                alignItems={'flex-end'}
+                justifyContent="center"
+              >
+                <Text variant={'mediumFont'}>{car?.vitesse}</Text>
+                <Text variant={'smallFont'}>Km/h</Text>
+              </Flex>
+              <Flex
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent="center"
+                h={'50px'}
+              >
+                <Text variant={'tinyFont'}>vitesse</Text>
+                <Text variant={'tinyFont'}>maximale</Text>
+              </Flex>
+            </Flex>
+            <Flex flexDirection={'column'}>
+              <Flex
+                flexDirection={'row'}
+                alignItems={'flex-end'}
+                justifyContent="center"
+              >
+                <Text variant={'mediumFont'}>{car?.zerocent}</Text>
+                <Text variant={'smallFont'}>s</Text>
+              </Flex>
+              <Flex
+                flexDirection={'column'}
+                alignItems={'center'}
+                justifyContent="center"
+                h={'50px'}
+              >
+                <Text variant={'tinyFont'} textAlign="cetner">
+                  0 à 100 km/h
+                </Text>
+              </Flex>
+            </Flex>
+          </Flex>
+          <Flex flexDirection={'column'} w={'100%'} gap={4}>
+            <Text variant={'menuLink'}>Propulsion</Text>
+            <SelectCar carNb={0} />
+            <Text variant={'menuLink'}>Transmission intégrale Dual Motor</Text>
+            <SelectCar carNb={1} />
+            <SelectCar carNb={2} />
+            <Flex flexDirection={'column'} alignItems="center">
+              <Text textAlign={'center'} variant={'nolinkText'}>
+                Les véhicules Tesla
+              </Text>
+              <Text textAlign={'center'} variant={'linkText'} mt={-1}>
+                appartiennent à la classe énergétique A{' '}
+              </Text>
+              <Text textAlign={'center'} variant={'nolinkText'}>
+                en matière d'émissions de CO2
+              </Text>
+              <Button mt={5} variant={'btnSmall'}>
+                Caractéristiques
+              </Button>
+            </Flex>
+          </Flex>
+          {state?.selectedCar === 2 ? <PerformanceDetail /> : ''}{' '}
+          <Flex
+            flexDirection={'column'}
+            alignItems="center"
+            gap={5}
+            my={'20vh'}
+          >
+            <Heading variant={'headModelsub'}>Couleur</Heading>
+            <SelectColors
+              colorsCar={colorsCar}
+              selectedCar={selectedCar}
+              setSelectedColor={setSelectedColor}
+              selectedColor={selectedColor}
+              state={state}
+            />
+          </Flex>
+          <Flex
+            flexDirection={'column'}
+            alignItems="center"
+            gap={5}
+            mb={'10vh'}
+          >
+            <Heading variant={'headModelsub'}>Jantes</Heading>
+            <SelectedWheels
+              wheelscar={wheelscar}
+              selectedCar={selectedCar}
+              setSelectedColor={setSelectedColor}
+              selectedColor={selectedColor}
+              state={state}
+            />
           </Flex>
         </Flex>
-        {state?.selectedCar === 2 ? <PerformanceDetail /> : ''}{' '}
-        <Flex flexDirection={'column'} alignItems="center" gap={5} my={'20vh'}>
-          <Heading variant={'headModelsub'}>Couleur</Heading>
-          <SelectColors
-            colorsCar={colorsCar}
-            selectedCar={selectedCar}
-            setSelectedColor={setSelectedColor}
-            selectedColor={selectedColor}
-            state={state}
-          />
-        </Flex>
-        <Flex flexDirection={'column'} alignItems="center" gap={5} mb={'10vh'}>
-          <Heading variant={'headModelsub'}>Jantes</Heading>
-          <SelectedWheels
-            wheelscar={wheelscar}
-            selectedCar={selectedCar}
-            setSelectedColor={setSelectedColor}
-            selectedColor={selectedColor}
-            state={state}
-          />
-        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
---
+++ src/component/ui/MenuConfig.jsx
@@ -0,0 +1,40 @@
+import {CloseIcon} from '@chakra-ui/icons'
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Text,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, {useEffect, useRef} from 'react'
+import './Mainmenu.css'
+export default function MenuConfig() {
+  return (
+    <>
+      <Flex
+        position={'fixed'}
+        w={'100vw'}
+        zIndex={99}
+        backdropFilter={'blur(10px)'}
+        bg={'#fffffff90'}
+        flexDirection="row"
+        px={{base: 5, xl: 20}}
+        justifyContent={'space-between'}
+        py={5}
+      >
+        <Flex justifyContent={'flex-start'} w={'25%'}>
+          <Box>
+            <Image src="/images/logo-b.png" width={120} height={50} />
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  )
+}
